75d81a2356fccf7c597b8a8c3eb46eafc5ed0207
aeroshev <aeroshev@noreply>
2023-01-05 17:16:29 +0300

    Описал все модели

--- src/project/models.py
+++ src/project/models.py
@@ -3,9 +3,11 @@ from datetime import datetime
 
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.contrib.postgres.fields import ArrayField
 
 
 class UserRole(models.TextChoices):
+    """Роли пользователей"""
     CHIEF = "Chief", "Начальник"
     TESTER = "Tester", "Тестировщик"
     DEVELOPER = "Developer", "Разработчик"
@@ -23,7 +25,7 @@ class User(AbstractUser):
         return f"{self.id} - {UserRole(self.role).label}"
 
     class Meta:
-        db_table = 'user'
+        db_table = 'users'
         constraints = [
             models.CheckConstraint(
                 name="%(app_label)s_%(class)s_role_valid",
@@ -33,6 +35,7 @@ class User(AbstractUser):
 
 
 class ProjectStatus(models.TextChoices):
+    """Статусы проекта"""
     CREATED = "Created", "Создан"
     EDITED = "Edited", "Редактируется"
     FINISHED = "Finished", "Завершён"
@@ -45,6 +48,8 @@ class Project(models.Model):
     status = models.CharField(max_length=128, choices=ProjectStatus.choices)  # type: str
     manager = models.ForeignKey(
         User,
+        related_name='projects',
+        related_query_name='project',
         on_delete=models.CASCADE
     )  # type: User
     created_at = models.DateTimeField(auto_created=True)  # type: datetime
@@ -54,7 +59,7 @@ class Project(models.Model):
         return f"{self.id} - {ProjectStatus(self.status).label}"
 
     class Meta:
-        db_table = 'project'
+        db_table = 'projects'
         constraints = [
             models.CheckConstraint(
                 name="%(app_label)s_%(class)s_status_valid",
@@ -64,6 +69,7 @@ class Project(models.Model):
 
 
 class SuiteStatus(models.TextChoices):
+    """Статусы тестового набора"""
     CREATED = "Created", "Создан"
     EDITED = "Edited", "Редактируется"
     FINISHED = "Finished", "Завершён"
@@ -76,6 +82,8 @@ class TestSuite(models.Model):
     status = models.CharField(max_length=128, choices=SuiteStatus.choices)
     project = models.ForeignKey(
         Project,
+        related_name='test_suites',
+        related_query_name='test_suite',
         on_delete=models.CASCADE
     )  # type: Project
     created_at = models.DateTimeField(auto_created=True)  # type: datetime
@@ -85,10 +93,101 @@ class TestSuite(models.Model):
         return f"{self.id} - {SuiteStatus(self.status).label}"
 
     class Meta:
-        db_table = 'test_suite'
+        db_table = 'test_suites'
         constraints = [
             models.CheckConstraint(
                 name="%(app_label)s_%(class)s_status_valid",
                 check=models.Q(status__in=SuiteStatus.values),
             )
         ]
+
+
+class RunStatus(models.TextChoices):
+    """Статусы запуска"""
+    CREATED = "Created", "Создан"
+    RUNNING = "Running", "Выполняется"
+    SUCCESS = "Success", "Успешно"
+    FAILED = "Failed", "Неудачно"
+
+
+class TestRun(models.Model):
+    """Объект запуска тестового набора"""
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)  # type: UUID
+    name = models.CharField(max_length=512)  # type: str
+    status = models.CharField(max_length=128, choices=RunStatus.choices)  # type: str
+    suite = models.ForeignKey(
+        TestSuite,
+        null=True,
+        related_name='test_runs',
+        related_query_name='test_run',
+        on_delete=models.SET_NULL
+    )  # type: TestSuite
+    project = models.ForeignKey(
+        Project,
+        null=True,
+        related_name='test_runs',
+        related_query_name='test_run',
+        on_delete=models.SET_NULL
+    )  # type: Project
+    running_by = models.ForeignKey(
+        User,
+        null=True,
+        related_name='test_runs',
+        related_query_name='test_run',
+        on_delete=models.SET_NULL
+    )  # type: User
+    created_at = models.DateTimeField(auto_created=True)  # type: datetime
+    changed_at = models.DateTimeField(auto_now=True)  # type: datetime
+
+    class Meta:
+        db_table = 'test_runs'
+        constraints = [
+            models.CheckConstraint(
+                name="%(app_label)s_%(class)s_status_valid",
+                check=models.Q(status__in=RunStatus.values),
+            )
+        ]
+
+
+class TestCase(models.Model):
+    """Тестовый набор"""
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)  # type: UUID
+    name = models.CharField(max_length=512)  # type: str
+    executed_code = models.IntegerField()  # type: int
+    conditions = ArrayField(
+        models.CharField(max_length=512),
+        default=list
+    )  # type: list[str]
+    steps = ArrayField(
+        models.CharField(max_length=512),
+        default=list
+    )  # type: list[str]
+    test_suites = models.ManyToManyField(
+        TestSuite,
+        related_name='test_cases',
+        related_query_name='test_case'
+    )  # type: list[TestSuite]
+    created_at = models.DateTimeField(auto_created=True)  # type: datetime
+    changed_at = models.DateTimeField(auto_now=True)  # type: datetime
+
+    class Meta:
+        db_table = 'test_cases'
+
+
+class Report(models.Model):
+    """Отчёты о тестировании"""
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)  # type: UUID
+    name = models.CharField(max_length=512)  # type: str
+    description = models.TextField()  # type: str
+    test_run = models.ForeignKey(
+        TestRun,
+        null=True,
+        related_name='reports',
+        related_query_name='report',
+        on_delete=models.SET_NULL
+    )  # type: TestRun
+    created_at = models.DateTimeField(auto_created=True)  # type: datetime
+    changed_at = models.DateTimeField(auto_now=True)  # type: datetime
+
+    class Meta:
+        db_table = 'reports'
