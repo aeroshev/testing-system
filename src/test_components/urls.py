dacf04d662f6e7f877299df5e9d1430083112bd6
aeroshev <aeroshev@noreply>
2023-01-13 19:23:35 +0300

    Прогресс по тестовым наборам

---
+++ src/test_components/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path
+
+urlpatterns = [
+    path('execute/', , name='tests_execute')
+]
--- src/testflow/urls.py
+++ src/testflow/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('project/', include('project.urls')),
     path('user/', include('user.urls')),
+    # path('tests/', include('test_components.urls')),
     path('', redirect_to_start, name='index'),
     path('start/', login_page, name='start')
 ]
