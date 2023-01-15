8f220d16c0b367627ec6f1d2cc23d4da687473dd
Mikhailpn <55245155+Mikhailpn@users.noreply.github.com>
2023-01-15 18:51:23 +0300

    Правки html и другие

--- src/test_components/urls.py
+++ src/test_components/urls.py
@@ -7,13 +7,15 @@ from .views import (
     get_test_case_page,
     get_test_plan_page,
     get_test_suite_page,
+    get_edit_case_page
 )
 
 urlpatterns = [
     path('plan/project/<uuid:project_id>/', get_test_plan_page, name='test_plan'),
     path('report/project/<uuid:project_id>/', get_report_page, name='test_report'),
     path('suites/project/<uuid:project_id>/', get_test_suite_page, name='test_suites'),
-    path('suites/edit/<uuid:suite_id>/', get_edit_suite_page, name='edit_suite'),
     path('suites/create/project/<uuid:project_id>/', create_test_suite, name='create_suite'),
-    path('cases/', get_test_case_page, name='test_cases')
+    path('cases/project/<uuid:project_id>/', get_test_case_page, name='test_cases'),
+    path('cases/edit/<uuid:testcase_id>/', get_edit_case_page, name='edit_case'),
+    path('suites/edit/<uuid:suite_id>/', get_edit_suite_page, name='edit_suite')
 ]
